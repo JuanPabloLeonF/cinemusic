b17bcb5182566f62575ea555a1b3e3d1155e1976
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-05-19 11:03:24 -0500

    agregando el servicio de traudccion basando me en los archivos .json de i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { TranslateServiceUtils } from './domain/services/translate.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'cinemusic';
+export class AppComponent implements OnInit {
+
+  protected translateServicesUtils:TranslateServiceUtils = inject(TranslateServiceUtils);
+
+  ngOnInit(): void {
+    this.translateServicesUtils.changeLanguage("es");
+  }
 }
