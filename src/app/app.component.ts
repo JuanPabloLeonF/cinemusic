14ae3cf8de839ff53c666969686b56d023d64098
Juan Pablo Leon Fonseca <fonseca123leon@gmail.com>
2025-10-02 15:31:42 -0500

    creando sub rutas como myMusic para la pages music y mejorando el responsive e dispositivos moviles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarMainComponent } from "./ui/layouts/nav-bar-main/nav-bar-main.component";
 import { StatesNavbarMainService } from './domain/states/states-navbar-main.service';
 import { MusicPlayerComponent } from "./ui/pages/music/music-player/music-player.component";
+import { NgStyle } from '@angular/common';
+import { StateMusicService } from './domain/states/music/state-music.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarMainComponent, MusicPlayerComponent],
+  imports: [RouterOutlet, NavBarMainComponent, MusicPlayerComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  protected stateMusic: StateMusicService = inject(StateMusicService);
   protected stateNavBarMainService: StatesNavbarMainService = inject(StatesNavbarMainService);
 }
